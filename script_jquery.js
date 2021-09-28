ce0a694d2dc0b744da5db5caa257b89d2a004d44
Thiago Reis <reist36@gmail.com>
2021-09-27 21:57:55 -0300

    creating the first commit of the beggining of the project of majukids.

---
+++ script_jquery.js
@@ -0,0 +1,21 @@
+$(function(){
+	$("#menino").click(function(){
+		$("h1").css("color", "#00e6b8");
+	});
+	$("#menina").click(function(){
+		$("h1").css("color", "#ff0066");
+	});
+	$("#promo").click(function(){
+		$("h1").css("color", "#ffa600");
+	});
+	$("#promo").click(function(){
+		$("#produtos").css("background-color", "#ffd280");
+	});
+	$("#menino").click(function(){
+		$("#produtos").css("background-color", "#99ffeb");
+	});
+	$("#menina").click(function(){
+		$("#produtos").css("background-color", "#ffccff");
+
+	});
+});
