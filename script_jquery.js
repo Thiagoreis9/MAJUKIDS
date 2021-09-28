f5dba5b87a819d7f6284a531e23a1a51aba8a6af
Thiago Reis <reist36@gmail.com>
2021-09-28 02:23:30 -0300

    adding the main block of product's of site, with 3 option of category's

--- script_jquery.js
+++ script_jquery.js
@@ -1,21 +1,30 @@
 $(function(){
+	$("#promo").click(function(){
+		$("h1").css("color", "#ffa600");
+		$("#produtopromo").css("display", "block");
+		$("#produtomeninos").css("display", "none");
+		$("#produtomeninas").css("display", "none");
+		$("#produtopromo").css("background-color", "#ffd280");
+	});
 	$("#menino").click(function(){
 		$("h1").css("color", "#00e6b8");
+		$("#produtopromo").css("display", "none");
+		$("#produtomeninos").css("display", "block");
+		$("#produtomeninas").css("display", "none");
+		$("#produtomeninos").css("background-color", "#99ffeb");
+		
 	});
 	$("#menina").click(function(){
 		$("h1").css("color", "#ff0066");
+		$("#produtopromo").css("display", "none");
+		$("#produtomeninos").css("display", "none");
+		$("#produtomeninas").css("display", "block");
+		$("#produtomeninas ").css("background-color", "#ffccff");
 	});
-	$("#promo").click(function(){
-		$("h1").css("color", "#ffa600");
-	});
-	$("#promo").click(function(){
-		$("#produtos").css("background-color", "#ffd280");
-	});
-	$("#menino").click(function(){
-		$("#produtos").css("background-color", "#99ffeb");
-	});
-	$("#menina").click(function(){
-		$("#produtos").css("background-color", "#ffccff");
+	
+	
 
-	});
+
+
+	
 });
